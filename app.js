d3d538fabb4ae3f3e8dd64dc35105794f263d608
CJ R <hello@cjr.co.de>
2016-11-16 15:45:29 -0700

    its been a long day

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.user(cors());
+app.use(cors());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
